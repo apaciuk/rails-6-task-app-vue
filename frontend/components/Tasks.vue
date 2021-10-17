4f1e6941a55159270f3413b589f25428616086a3
Paul Anthony McGowan <xhostcom@gmail.com>
2021-10-17 17:45:51 +0100

    Tasks component

---
+++ frontend/components/Tasks.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+  <h1>Tasks</h1>
+  <div class="legend">
+   <span>Double click to mark as complete</span>
+ </div>
+ </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,13 +1,14 @@
 <template>
-  <h1>Front</h1>
+ <div class="container">
+  <h1 class="title is-1 has-text-centered pt-5">Task Manager</h1>
+  <h3 class="subtitle has-text-centered pt-3">Powered by Ruby on Rails 6, Vue JS, Nuxt, PostgreSQL</h3>
+  <Tasks />
+  </div>
 </template>
-
 <script>
 
 
 export default {
-  name: 'HomePage',
-
-
+  name: 'HomePage'
 }
 </script>
